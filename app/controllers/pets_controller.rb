64ca4e238bc6e5e3fd6c2c0f4ba8b3aeec66c140
Eric Storey <estorey@gmail.com>
2019-05-06 07:07:40 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -13,7 +13,7 @@ class PetsController < ApplicationController
   post '/pets' do 
     @pet = Pet.create(params[:pet])
     if params[:pet][:owner_name] != ""
-      @pet.owner=Owner.new(name: params[:pet][:owner_name])
+      @pet.owner=Owner.new(name: params[:owner_name])
     end
     @pet.save
     redirect to "pets/#{@pet.id}"
